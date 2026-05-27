dbd11b9c449cc995aa2cf233d311cdf2f1ee6596
Joach27 <reekgold26@gmail.com>
2026-05-28 00:34:50 +0100

    Remove commented database creation and drop lines from main execution block

--- app/app.py
+++ app/app.py
@@ -215,12 +215,11 @@ def test_relation():
     }), 200
 
 # with app.app_context():
-#     db.drop_all()
 #     db.create_all()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
+    # with app.app_context():
+    #     db.drop_all()
+    #     db.create_all()
     app.run(debug=True)
 
